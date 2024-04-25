09f4ced6441d877a521743c39690da5084a93eed
junioradmin <Linus@freistetter.com>
2024-04-25 10:39:22 +0200

    mmmm yummy excel

---
+++ src/main/java/iputils/IPAddress.java
@@ -0,0 +1,92 @@
+package iputils;
+
+import java.util.Objects;
+
+public class IPAddress implements Comparable<IPAddress> {
+
+    /**
+     * ip as integer
+     */
+    private int ip;
+
+    /**
+     * create IP from 4 Numbers
+     *
+     * @param a3
+     * @param a2
+     * @param a1
+     * @param a0
+     */
+    public IPAddress(int a3, int a2, int a1, int a0) {
+        createIP(a3, a2, a1, a0);
+    }
+
+    /**
+     * create IP from given integer (internal use)
+     *
+     * @param ip
+     */
+    public IPAddress(int ip) {
+        this.ip = ip;
+    }
+
+    public IPAddress(String ip) {
+        String[] nums = ip.split("\\.");
+        if (nums.length != 4) {
+            throw new IllegalArgumentException("ill formed ip");
+        }
+        createIP(Integer.parseInt(nums[0]), Integer.parseInt(nums[1]), Integer.parseInt(nums[2]), Integer.parseInt(nums[3]));
+    }
+
+    /**
+     * create IP/Netmask with given number of bits
+     *
+     * @param cidr number of bits
+     * @return
+     */
+    public static IPAddress createNetmask(int cidr) {
+        int mask = (int) (0xffffffff00000000L >> cidr);
+        // System.out.format("cm %2d: %08x\n", cidr, mask);
+        return new IPAddress(mask);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IPAddress ipAddress = (IPAddress) o;
+        return ip == ipAddress.ip;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ip);
+    }
+
+    private void createIP(int a3, int a2, int a1, int a0) {
+        this.ip = (a3 << 24) + (a2 << 16) + (a1 << 8) + a0;
+        // System.out.format("%d.%d.%d.%d: %08x\n", a3,a2,a1,a0,ip);
+    }
+
+    /**
+     * @return ip address as integer
+     */
+    public int getIP() {
+        return ip;
+    }
+
+    @Override
+    public String toString() {
+        int a0 = (ip) & 0xff;
+        int a1 = (ip >>> 8) & 0xff;
+        int a2 = (ip >>> 16) & 0xff;
+        int a3 = (ip >>> 24) & 0xff;
+
+        return "IPAddress [" + a3 + "." + a2 + "." + a1 + "." + a0 + "]";
+    }
+
+    @Override
+    public int compareTo(IPAddress o) {
+        return this.ip - o.ip;
+    }
+}
---
+++ src/main/java/tests/Tests.java
@@ -0,0 +1,89 @@
+package tests;
+
+import iputils.IPAddress;
+import iputils.Subnet;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFChart;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
+public class Tests {
+    private static void testSet(Set<IPAddress> sIP) {
+        sIP.add(new IPAddress("10.0.0.1"));
+        sIP.add(new IPAddress("10.0.0.2"));
+        sIP.add(new IPAddress("10.0.0.1"));
+        System.out.println("sIP.size() = " + sIP.size()); // IntelliJ: soutv
+        for (IPAddress ip : sIP) {
+            System.out.println("ip = " + ip);
+        }
+    }
+
+    public static void main(String[] args) {
+        testSet(new HashSet<>());
+        testSet(new TreeSet<>());
+        perfTest(new Subnet("192.168.0.0/24"), new HashSet<>());
+        toExcel();
+    }
+
+    private static Map<Integer, Long> perfTest(Subnet s, Set<IPAddress> sIP) {
+        long start = System.currentTimeMillis();
+        int cidr = Integer.toBinaryString(s.getMask().getIP()).replaceAll("0", "").length();
+        for (int i = 0; i < Math.pow(2, 32 - cidr); i++) {
+            sIP.add(new IPAddress(s.getNet().getIP() + i));
+        }
+        for (int i = 0; i < Math.pow(2, 32 - cidr) * 2; i++) {
+            sIP.contains(new IPAddress(s.getNet().getIP() + i));
+        }
+        HashMap<Integer, Long> h = new HashMap<>();
+        h.put(cidr, System.currentTimeMillis() - start);
+        return h;
+    }
+
+    public static void toExcel() {
+        HashMap<Integer, Long> all = new HashMap<>();
+        for (int i = 0; i < 32; i++) {
+            Subnet s = new Subnet("0.0.0.0/" + i);
+            all.putAll(perfTest(s, new HashSet<>()));
+        }
+
+        try {
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Chart sheet");
+
+            // Sample data
+
+            // Create a row and put some cells in it. Rows are 0 based.
+            Row row = sheet.createRow(0);
+            Cell cell = row.createCell(0);
+            cell.setCellValue("SNM");
+            cell = row.createCell(1);
+            cell.setCellValue("Duration");
+
+            int rownum = 1;
+            for (Map.Entry<Integer, Long> entry : all.entrySet()) {
+                row = sheet.createRow(rownum++);
+                cell = row.createCell(0);
+                cell.setCellValue(entry.getKey());
+                cell = row.createCell(1);
+                cell.setCellValue((double) entry.getValue()/100);
+            }
+
+            Drawing<?> drawing = sheet.createDrawingPatriarch();
+            try (OutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
+                workbook.write(fileOut);
+            }
+
+            workbook.close();
+
+        }catch (IOException e){e.printStackTrace();}
+
+    }
+}
+
